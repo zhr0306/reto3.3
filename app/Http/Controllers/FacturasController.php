ca813b0a66cc309b7f4cfac1fe69d600c54e21e7
Marria <maskastarik@gmail.com>
2026-02-01 15:53:47 +0100

    He cambiado FC methodo update

--- app/Http/Controllers/FacturasController.php
+++ app/Http/Controllers/FacturasController.php
@@ -75,7 +75,7 @@ class FacturasController extends Controller
 
         Facturas::where('id', '=', $id)->update($datos);
 
-        return redirect('facturas')->with('mensaje', "Factura modificada");
+        return redirect('facturas')->with('mensaje', "Factura modificada con exito ");
     }
 
     /**
